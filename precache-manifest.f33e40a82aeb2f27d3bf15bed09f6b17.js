c272cc970189d1c9b992b421f2bc4922ca2c299f
tanveer alam <mohammadtanveer0289@gmail.com>
2024-08-08 02:42:46 +0530

    Updates

---
+++ precache-manifest.f33e40a82aeb2f27d3bf15bed09f6b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e65a43eb09ad778fd57ba63035972e8",
+    "url": "/YouTube/index.html"
+  },
+  {
+    "revision": "c4e11a588182d92b204c",
+    "url": "/YouTube/static/css/main.b7655e1f.chunk.css"
+  },
+  {
+    "revision": "c7253f495449017d80ec",
+    "url": "/YouTube/static/js/2.8e49cd34.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/YouTube/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "c4e11a588182d92b204c",
+    "url": "/YouTube/static/js/main.3383a841.chunk.js"
+  },
+  {
+    "revision": "5c2e7c7b3e0dc9df5c55",
+    "url": "/YouTube/static/js/runtime~main.01e8aee4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "%7Balamtanveer%7D.github.io/%7BYouTube%7D/precache-manifest.8766a934299f993b412516338555fdf3.js"
+  "/YouTube/precache-manifest.f33e40a82aeb2f27d3bf15bed09f6b17.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("%7Balamtanveer%7D.github.io/%7BYouTube%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/YouTube/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
