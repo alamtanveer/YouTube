7131032768b8aa7accd4b71dcb5a8dd06773e094
tanveer alam <mohammadtanveer0289@gmail.com>
2024-08-08 02:00:23 +0530

    Updates

---
+++ precache-manifest.8766a934299f993b412516338555fdf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99751a05e21ba49ce5f6bf6c66b5454b",
+    "url": "%7Balamtanveer%7D.github.io/%7BYouTube%7D/index.html"
+  },
+  {
+    "revision": "c4e11a588182d92b204c",
+    "url": "%7Balamtanveer%7D.github.io/%7BYouTube%7D/static/css/main.b7655e1f.chunk.css"
+  },
+  {
+    "revision": "c7253f495449017d80ec",
+    "url": "%7Balamtanveer%7D.github.io/%7BYouTube%7D/static/js/2.8e49cd34.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "%7Balamtanveer%7D.github.io/%7BYouTube%7D/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "c4e11a588182d92b204c",
+    "url": "%7Balamtanveer%7D.github.io/%7BYouTube%7D/static/js/main.3383a841.chunk.js"
+  },
+  {
+    "revision": "15f0d950f99631c9a79f",
+    "url": "%7Balamtanveer%7D.github.io/%7BYouTube%7D/static/js/runtime~main.de9e6853.js"
+  }
+]);
