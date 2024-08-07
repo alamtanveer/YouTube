0fd6d9cae78ef27e1ad97803eb771ce0bd1cd321
tanveer alam <mohammadtanveer0289@gmail.com>
2024-08-08 01:09:24 +0530

    youtube clone App

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+const GOOGLE_API_KEY = "AIzaSyAS0ybolsH2KHY8ntSWAN-fEwNexcbIEVw";
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
